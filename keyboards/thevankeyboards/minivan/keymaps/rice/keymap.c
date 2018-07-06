c1a7ecda3b865a05dca97193237ff2228a93ebe1
Tristan Rice <rice@fn.lc>
2018-07-06 12:20:53 -0400

    fixed tv44 keymap after rebase

--- keyboards/thevankeyboards/minivan/keymaps/rice/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/rice/keymap.c
@@ -8,6 +8,7 @@
 
 extern keymap_config_t keymap_config;
 
+#undef LAYOUT
 #define LAYOUT( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
