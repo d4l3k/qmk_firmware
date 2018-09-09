309da818241042f6850ca7dc125001bf707cf2ca
Tristan Rice <rice@fn.lc>
2018-09-08 19:51:15 -0700

    levinson: set up rgb animations

--- keyboards/levinson/keymaps/rice/config.h
+++ keyboards/levinson/keymaps/rice/config.h
@@ -31,3 +31,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_LEFT
 #define MASTER_RIGHT
 // #define EE_HANDS
+
+
+#define RGBLIGHT_ANIMATIONS
+#undef RGBLED_NUM
+#define RGBLED_NUM 6     // Number of LEDs in your strip
